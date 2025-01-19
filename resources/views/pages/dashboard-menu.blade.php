c1a810b9e142700cce1bd89cba7601714d5b4049
jpdzsoftware <jeremyivanpedraza@gmail.com>
2025-01-19 12:39:49 -0500

    actualización de front y estilos responsivos

--- resources/views/pages/dashboard-menu.blade.php
+++ resources/views/pages/dashboard-menu.blade.php
@@ -1,37 +1,36 @@
 @push('styles')
     <link rel="stylesheet" href="{{ asset('assets/css/dashboard.min.css') }}">
 @endpush
+@section('title', 'Dashboard')
 <x-app-layout>
     <main class="main">
-        <section id="menu-cards" class="hero section">
+        <section style="background-image: url('{{asset('assets/img/hero-bg.png')}}')" id="menu-cards" class="hero section">
             <article class="card">
-                <img class="card__background" src="https://colombia.travel/sites/default/files/styles/imagen_650x450_escala_y_recorte/public/destino/shutterstock_2200949319%20%282%29%20%281%29.jpg?itok=SxWtDXg_"
-                    alt="Photo of Cartagena's cathedral at the background and some colonial style houses" width="1920"
+                <img class="card__background" src="https://infowan.net/Content/images/support/supportticket.png"
+                    alt="Tools Photo" width="1920"
                     height="2193" />
                 <div class="card__content | flow">
                     <div class="card__content--container | flow">
-                        <h2 class="card__title">Colombia</h2>
+                        <h2 class="card__title"><i class="fa-solid fa-screwdriver-wrench"></i> Tools</h2>
                         <p class="card__description">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
-                            labore laudantium deserunt fugiat numquam.
+                            Herramientas para automatizar procesos
                         </p>
                     </div>
-                    <button class="card__button">Read more</button>
+                    <button class="card__button">Ingresar</button>
                 </div>
             </article>
             <article class="card">
-                <img class="card__background" src="https://colombia.travel/sites/default/files/styles/imagen_650x450_escala_y_recorte/public/destino/shutterstock_2200949319%20%282%29%20%281%29.jpg?itok=SxWtDXg_"
-                    alt="Photo of Cartagena's cathedral at the background and some colonial style houses" width="1920"
+                <img class="card__background" src="https://support.cc/images/blog/it-ticketing-tools.png?v=1682512742702523116"
+                    alt="Tickets SoftlogyDesk" width="1920"
                     height="2193" />
                 <div class="card__content | flow">
                     <div class="card__content--container | flow">
-                        <h2 class="card__title">Colombia</h2>
+                        <h2 class="card__title"><i class="fa-solid fa-ticket"></i> Tickets</h2>
                         <p class="card__description">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
-                            labore laudantium deserunt fugiat numquam.
+                            Solicite Soporte Técnico en pocos pasos
                         </p>
                     </div>
-                    <button class="card__button">Read more</button>
+                    <button class="card__button">Ingresar</button>
                 </div>
             </article>
         </section>
